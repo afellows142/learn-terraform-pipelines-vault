1aae44c3d711965afe1667a5bebb2b22b51e01e5
afellows142 <67278983+afellows142@users.noreply.github.com>
2020-06-22 11:54:32 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "consul_workspace" {
   type        = string
+  default     = "andrew-f-consul"
   description = "Workspace that created the Consul cluster"
 }
 
 variable "cluster_workspace" {
   type        = string
+  default     = "andrew-f-k8s"
   description = "Workspace that created the Kubernetes cluster"
 }
 
 variable "organization" {
   type        = string
+  default     = "infrastructure-pipelines-work"
   description = "Organization of workspace that created the Kubernetes cluster"
 }
